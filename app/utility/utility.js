db34f00707216f4005a88eef198760bda00925a7
montalbo <oseppe@users.noreply.github.com>
2017-11-13 17:22:07 +0800

    Add level in competitionData

--- app/utility/utility.js
+++ app/utility/utility.js
@@ -7,12 +7,14 @@ function buildCompetitionData(competitors) {
 	let currentMatch = 1;
 	let matchContestantCounter = 1;
 	let competitorId = 0;
+	let currentLevel = 0;
 
 	// build the competitor cards for first level
 	for(let competitor of competitors) {
 		const competitorData = {};
 
 		competitorData.id = `${competitorId}`;
+		competitorData.level = currentLevel;
 		competitorData.name = competitor;
 		competitorData.score = 0;
 		competitorData.matchNumber = currentMatch;
@@ -31,12 +33,16 @@ function buildCompetitionData(competitors) {
 
 	const totalCompetitorCards = (competitors.length - 1) * 2;
 	let remainingCompetitorCards = totalCompetitorCards - competitors.length;
+	let competitorCountPerLevel = competitors.length / 2;
+	let remainingCompetitorsInLevel = competitorCountPerLevel;
+	currentLevel++;
 
 	// build the competitor cards for the succeeding levels
 	for (let i = remainingCompetitorCards; i > 0; i--) {
 		const competitorData = {};
 
 		competitorData.id = '';
+		competitorData.level = currentLevel;
 		competitorData.name = '';
 		competitorData.score = 0;
 		competitorData.matchNumber = currentMatch;
@@ -49,7 +55,15 @@ function buildCompetitionData(competitors) {
 			matchContestantCounter = 1;
 		}
 
-		competitionData.push(competitorData);	
+		competitionData.push(competitorData);
+
+		remainingCompetitorsInLevel--;
+
+		if (remainingCompetitorsInLevel === 0) {
+			competitorCountPerLevel = competitorCountPerLevel / 2;
+			remainingCompetitorsInLevel = competitorCountPerLevel;
+			currentLevel++;
+		}
 	}
 
 	return competitionData;
