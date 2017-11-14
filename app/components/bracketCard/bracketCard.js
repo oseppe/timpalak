934c3708780c133aacddae74531115e2716c2982
montalbo <oseppe@users.noreply.github.com>
2017-11-14 15:46:45 +0800

    Use props instead of status in bracketCard

--- app/components/bracketCard/bracketCard.js
+++ app/components/bracketCard/bracketCard.js
@@ -2,37 +2,19 @@ import React, { Component } from 'react'
 import MatchCard from '../matchCard/matchCard'
 
 class BracketCard extends Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			firstMatch: [this.props.bracketCompetitors[0], this.props.bracketCompetitors[1]],
-			secondMatch: [this.props.bracketCompetitors[2], this.props.bracketCompetitors[3]],
-			firstMatchNumber: this.props.bracketCompetitors[0].matchNumber,
-			secondMatchNumber: this.props.bracketCompetitors[2].matchNumber,
-		}
-	}
-
-	componentWillReceiveProps(nextProps) {
-		this.setState({
-			firstMatch: [this.props.bracketCompetitors[0], this.props.bracketCompetitors[1]],
-			secondMatch: [this.props.bracketCompetitors[2], this.props.bracketCompetitors[3]],
-		});
-	}
-
 	render() {
 		return(
 			<div  style={{}}>
 				<div style={{}}>
-					<MatchCard matchCompetitors={this.state.firstMatch} 
-							matchNumber={this.state.firstMatchNumber}
+					<MatchCard matchCompetitors={[this.props.bracketCompetitors[0], this.props.bracketCompetitors[1]]} 
+							matchNumber={this.props.bracketCompetitors[0].matchNumber}
 							onCompetitorCardChangeName={this.props.onCompetitorCardChangeName}
 							onCompetitorCardHover={this.props.onCompetitorCardHover}
 							onCompetitorMouseLeave={this.props.onCompetitorMouseLeave}
 							onCompetitorCardChangeScore={this.props.onCompetitorCardChangeScore}
 							onMatchFight={this.props.onMatchFight} />
-					<MatchCard matchCompetitors={this.state.secondMatch} 
-							matchNumber={this.state.secondMatchNumber}
+					<MatchCard matchCompetitors={[this.props.bracketCompetitors[2], this.props.bracketCompetitors[3]]} 
+							matchNumber={this.props.bracketCompetitors[2].matchNumber}
 							onCompetitorCardChangeName={this.props.onCompetitorCardChangeName}
 							onCompetitorCardHover={this.props.onCompetitorCardHover}
 							onCompetitorMouseLeave={this.props.onCompetitorMouseLeave}
